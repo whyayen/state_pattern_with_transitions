e40072cad67817736179fb7d6f2ed9c253ccbf79
yanzijun <c75a90@gmail.com>
2020-09-21 14:48:48 +0800

    Add transition methods by define_method

--- context.rb
+++ context.rb
@@ -1,13 +1,45 @@
 require 'states/close_state'
 require 'states/lock_state'
 require 'states/open_state'
+require 'transitions/door_transitions.rb'
 
 class Context
   attr_accessor :state
 
   def initialize
     @state = OpenState.new
+
+    transitions = [
+      :close_door,
+      :lock_door,
+      :unlock_door,
+      :open_door
+    ]
+
+    from = {
+      close_door: [:open],
+      lock_door: [:close],
+      unlock_door: [:lock],
+      open_door: [:close]
+    }
+
+    to = {
+      close_door: 'CloseState',
+      lock_door: 'LockState',
+      unlock_door: 'CloseState',
+      open_door: 'OpenState'
+    }
+
+    transitions.each do |transition|
+      self.class.send(:define_method, transition) do |*args, &block|
+        if from[transition].include?(@state.value)
+          @state = Object.const_get(to[transition]).new
+        else
+          raise ArgumentError 'invalid state'
+        end
+      end
+    end
   end
-  
+
   # transitions
 end
